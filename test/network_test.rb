912c684cc9798e86611fefcfd794b55486676a07
Greg Mitchell <g2mitchell3003@gmail.com>
2020-09-01 09:56:20 -0600

    Create network class and test file and test it exists

---
+++ test/network_test.rb
@@ -0,0 +1,13 @@
+require './lib/show'
+require './lib/character'
+require './lib/network'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class NetworkTest < Minitest::Test
+  def test_it_exists
+    nbc = Network.new("NBC")
+
+    assert_instance_of Network, nbc
+  end
+end
