3e841dbe491249d79c5bb90cc25a2c48bcc2a62f
Greg Mitchell <g2mitchell3003@gmail.com>
2020-09-01 10:11:52 -0600

    add test and method for main characters

--- lib/network.rb
+++ lib/network.rb
@@ -8,4 +8,16 @@ class Network
   def add_show(show)
     @shows << show
   end
+
+  def main_characters
+    main_chars = []
+    @shows.each do |show|
+      show.characters.each do |character|
+        if character.name == character.name.upcase && character.salary > 500_000
+          main_chars << character
+        end
+      end
+    end
+    main_chars
+  end
 end
