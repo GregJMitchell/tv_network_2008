ce6ac52cd8d2e55fdf614c8f4e588b42f1045831
Greg Mitchell <g2mitchell3003@gmail.com>
2020-09-01 09:06:35 -0600

    Create character test and class file and test it exists

---
+++ test/character_test.rb
@@ -0,0 +1,11 @@
+require './lib/character'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class TestCharacter < Minitest::Test
+  def test_it_exists
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+
+    assert_instance_of Character, kitt
+  end
+end
